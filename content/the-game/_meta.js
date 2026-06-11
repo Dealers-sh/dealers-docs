69692c788cc91991db28471512188c90e6c36575
Berny <berny.ust@gmail.com>
2026-06-11 17:56:29 +0200

    Update docs, add heists

--- content/the-game/_meta.js
+++ content/the-game/_meta.js
@@ -1,11 +1,12 @@
 export default {
   overview: 'Overview',
-  stash: 'Stash',
+  stash: '📦 Stash',
   deal: '🤝 Deal (PVE)',
   move: '✈️ Move',
   pvp: '⚔️ PVP',
+  heists: '🚚 Heists',
   shop: ' 🛍️ Shop',
   pager: '📟 Pager',
   bank: '🏦 Bank',
-  jail: 'Jail'
+  jail: '🚨 Jail'
 }
