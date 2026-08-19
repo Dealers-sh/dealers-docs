ab9816204885a13a568597fe279f21594fd52668
Berny <berny.ust@gmail.com>
2026-08-19 20:05:18 +0200

    Add patch 1.3.0 details

--- content/contracts/_meta.js
+++ content/contracts/_meta.js
@@ -1,6 +1,5 @@
 export default {
   overview: 'Overview',
   addresses: 'Addresses',
-  abi: 'ABI & Source',
-  audit: 'Audit'
+  abi: 'ABI & Source'
 }
--- content/patch-notes/_meta.js
+++ content/patch-notes/_meta.js
@@ -5,6 +5,7 @@ export default {
       breadcrumb: false
     }
   },
+  'v1-3-0': 'v1.3.0 patch notes',
   'v1-2-0': 'v1.2.0 patch notes',
   'v1-1-0': 'v1.1.0 patch notes'
 }
